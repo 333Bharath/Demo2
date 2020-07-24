327d6e4e2adce0eaf7d664e1a2bb3ccd1eb898eb
333Bharath <60769837+333Bharath@users.noreply.github.com>
2020-07-24 10:50:51 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,31 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //string name;
-            //int age;
-
-            //Console.WriteLine("")
-            // Console.WriteLine("HEllO WORLD");
-            //Console.WriteLine("this is my first program");
-
-
-            // Console.WriteLine("enter the name");
-            //name = Console.ReadLine();
-            // Console.WriteLine("enter the age");
-            // age = Convert.ToInt32(Console.ReadLine());
-            // Console.WriteLine(name+" "+age);
-            // Console.ReadLine();
-
-            //int n1;
-            //int n2;
-            //int n3;
-
-            //Console.WriteLine("enter the n1");
-            //n1 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("enter the n2");
-            //n2 = Convert.ToInt32(Console.ReadLine());
-            //n3 = n1 + n2;
-            //Console.WriteLine(n3);
+            
 
             DateTime d1 = DateTime.Now;
 
@@ -43,8 +19,7 @@ namespace ConsoleApp1
             Console.WriteLine(d1.ToLongTimeString());
             Console.WriteLine(d1.ToLongTimeString());
             Console.WriteLine(d1.ToShortDateString());
-
-
+            
 
             Console.ReadKey();
 
